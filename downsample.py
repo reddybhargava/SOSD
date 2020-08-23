089b49c4c5d5d3d6c016e2e0e3c8294a246de098
Bhargava Reddy <reddybhargava@outlook.in>
2020-08-23 13:35:45 +0000

    Downsample only to 200M dataset

--- downsample.py
+++ downsample.py
@@ -10,14 +10,14 @@ def downsample(fn):
     d = np.fromfile("data/" + fn + "_800M_uint64", dtype=np.uint64)[1:]
     nd = np.delete(d, np.arange(0, d.size, 4))
 
-    with open("data/" + fn + "_600M_uint64", "wb") as f:
-        f.write(struct.pack("Q", len(nd)))
-        nd.tofile(f)
+    # with open("data/" + fn + "_600M_uint64", "wb") as f:
+    #     f.write(struct.pack("Q", len(nd)))
+    #     nd.tofile(f)
 
-    nd = d[::2]
-    with open("data/" + fn + "_400M_uint64", "wb") as f:
-        f.write(struct.pack("Q", len(nd)))
-        nd.tofile(f)
+    # nd = d[::2]
+    # with open("data/" + fn + "_400M_uint64", "wb") as f:
+    #     f.write(struct.pack("Q", len(nd)))
+    #     nd.tofile(f)
 
     nd = d[::4]
     with open("data/" + fn + "_200M_uint64", "wb") as f:
