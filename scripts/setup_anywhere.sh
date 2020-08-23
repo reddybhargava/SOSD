45a512ab62c83ef3622bb6a3124aa8f8ef8590ea
Bhargava Reddy <reddybhargava@outlook.in>
2020-08-23 13:32:21 +0000

    added apt update

--- scripts/setup_anywhere.sh
+++ scripts/setup_anywhere.sh
@@ -5,6 +5,7 @@ git submodule update
 
 echo "Installing software..."
 export DEBIAN_FRONTEND=noninteractive
+sudo apt update
 sudo apt install -y python3-pip cmake linux-tools-common linux-tools-aws libboost-all-dev m4 zstd
 pip3 install --user numpy scipy scikit-learn jupyterlab pandas matplotlib
 curl https://sh.rustup.rs -sSf | sh -s -- -y
